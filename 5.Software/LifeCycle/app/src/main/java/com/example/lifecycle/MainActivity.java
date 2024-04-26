cd2e38facd9edeb1397353cc28bbf592980a1c2c
mixiaojie <907638274@qq.com>
2024-04-26 17:09:43 +0800

    add 5.Software

---
+++ 5.Software/LifeCycle/app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.lifecycle;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Chronometer;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Chronometer chronometer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chronometer = findViewById(R.id.chronometer);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
